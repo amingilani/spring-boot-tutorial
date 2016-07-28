8ce651ee6696ca76eb4d5103ac350d74bc903e29
amingilani <aminshahgilani@gmail.com>
2016-07-28 12:32:34 +0500

    User registration saves the user in the db

--- v2/completed/src/main/java/myapp/controllers/RegistrationController.java
+++ v2/completed/src/main/java/myapp/controllers/RegistrationController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +28,9 @@ import javax.validation.Valid;
 @Controller
 public class RegistrationController {
 
+@Autowired
+private UserRepository repository;
+
 @RequestMapping(value = "/", method = RequestMethod.GET)
 public String showLoginForm() {
         return "registrations/login";
@@ -38,5 +42,22 @@ public String showRegistrationForm(WebRequest request, Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "registrations/register";
+}
+
+@RequestMapping(value = "/register", method = RequestMethod.POST)
+public String registerUserAccount(WebRequest request,
+                                        @ModelAttribute("user") @Valid User user,
+                                        BindingResult result) {
+        if (!result.hasErrors()) {
+                repository.save(user);
+
+                System.out.println("saved the user!");
+                System.out.println(
+                repository.findByUserName(user.getUserName() ));
+
+
+        }
+        return "redirect:/";
+
 }
 }
