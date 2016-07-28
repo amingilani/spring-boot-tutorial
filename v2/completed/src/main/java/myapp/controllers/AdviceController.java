e301d82e8baa9b38809d277dacbdbab71f960c90
amingilani <aminshahgilani@gmail.com>
2016-07-28 10:23:35 +0500

    Got advice to show via advice controller

---
+++ v2/completed/src/main/java/myapp/controllers/AdviceController.java
@@ -0,0 +1,35 @@
+package myapp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
+
+
+
+
+
+@Controller
+public class AdviceController {
+
+@RequestMapping(value = "/advice", method = RequestMethod.GET)
+public String showAdvice() {
+        return "advice";
+}
+
+}
