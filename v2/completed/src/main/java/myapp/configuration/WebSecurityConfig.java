3b9a6a2e27925b5c1a716ade2bb2ff51e28463f7
amingilani <aminshahgilani@gmail.com>
2016-07-29 02:25:26 +0500

    Cleanup unnecessary coma

--- v2/completed/src/main/java/myapp/configuration/WebSecurityConfig.java
+++ v2/completed/src/main/java/myapp/configuration/WebSecurityConfig.java
@@ -6,11 +6,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import myapp.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 @Override
 protected void configure(HttpSecurity http) throws Exception {
