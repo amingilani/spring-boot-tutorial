de441b8f6d47418d2a2a6330eb92792e5642ce31
amingilani <aminshahgilani@gmail.com>
2016-07-28 10:19:36 +0500

    Got login and register views to show via controller

---
+++ v2/completed/src/main/java/myapp/controllers/RegistrationController.java
@@ -0,0 +1,42 @@
+package myapp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
+
+
+
+
+
+@Controller
+public class RegistrationController {
+
+@RequestMapping(value = "/", method = RequestMethod.GET)
+public String showLoginForm() {
+        return "registrations/login";
+}
+
+
+@RequestMapping(value = "/register", method = RequestMethod.GET)
+public String showRegistrationForm(WebRequest request, Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "registrations/register";
+}
+}
